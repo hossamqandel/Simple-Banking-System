1d7641c88d9d8458d553a576eb6862430252b9fd
hossamqandel <63760755+hossamqandel@users.noreply.github.com>
2021-07-14 22:14:43 +0200

    Update Bank.java

--- src/Bank.java
+++ src/Bank.java
@@ -7,7 +7,7 @@ public class Bank {
 		BankATM user1 = new BankATM ("Adam",54684);
 		user1.showList();
 		
-		BankATM user2 = new BankATM ("Hossam",84684);
+		BankATM user2 = new BankATM ("Mohamed",84684);
 		user2.showList();
 		
 	}
